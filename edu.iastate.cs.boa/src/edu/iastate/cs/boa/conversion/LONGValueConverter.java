226efc7d7ab02e5f4e23f60cf294e8b7c5e0490c
Robert Dyer <rdyer@iastate.edu>
2014-05-26 18:36:19 -0500

    support binary ints

--- edu.iastate.cs.boa/src/edu/iastate/cs/boa/conversion/LONGValueConverter.java
+++ edu.iastate.cs.boa/src/edu/iastate/cs/boa/conversion/LONGValueConverter.java
@@ -1,5 +1,7 @@
 package edu.iastate.cs.boa.conversion;
 
+import java.math.BigInteger;
+
 import org.eclipse.xtext.conversion.ValueConverterException;
 import org.eclipse.xtext.conversion.impl.AbstractLexerBasedConverter;
 import org.eclipse.xtext.nodemodel.INode;
@@ -16,11 +18,12 @@ public class LONGValueConverter extends AbstractLexerBasedConverter<Long> {
 			throw new ValueConverterException("Couldn't convert empty string to a long value.", node, null);
 
 		try {
-			// FIXME doesnt handle binary
-			long val = Long.decode(string);
-			return Long.valueOf(val);
+			if (string.toLowerCase().startsWith("0b"))
+				return Long.valueOf(new BigInteger(string.substring(2), 2).longValue());
+
+			return Long.valueOf(Long.decode(string));
 		} catch (NumberFormatException e) {
-			throw new ValueConverterException("Couldn't convert '" + string + "' to an long value.", node, e);
+			throw new ValueConverterException("Couldn't convert '" + string + "' to a long value.", node, e);
 		}
 	}
 }
