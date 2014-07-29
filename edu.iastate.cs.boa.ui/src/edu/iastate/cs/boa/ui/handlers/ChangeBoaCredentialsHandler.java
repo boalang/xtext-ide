e65b9ee8d02d55eb55697eafca261a9db9f5148c
Robert Dyer <rdyer@bgsu.edu>
2014-07-29 16:49:50 -0400

    update client API jar (which caches datasets itself) and remove dataset caching from the IDE

--- edu.iastate.cs.boa.ui/src/edu/iastate/cs/boa/ui/handlers/ChangeBoaCredentialsHandler.java
+++ edu.iastate.cs.boa.ui/src/edu/iastate/cs/boa/ui/handlers/ChangeBoaCredentialsHandler.java
@@ -1,6 +1,7 @@
 /*
- * Copyright 2014, Hridesh Rajan, Robert Dyer, Sambhav Srirama
- *                 and Iowa State University of Science and Technology
+ * Copyright 2014, Hridesh Rajan, Robert Dyer, Sambhav Srirama,
+ *                 Iowa State University of Science and Technology
+ *                 and Bowling Green State University
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- edu.iastate.cs.boa.ui/src/edu/iastate/cs/boa/ui/handlers/SubmitToBoaHandler.java
+++ edu.iastate.cs.boa.ui/src/edu/iastate/cs/boa/ui/handlers/SubmitToBoaHandler.java
@@ -1,6 +1,7 @@
 /*
- * Copyright 2014, Hridesh Rajan, Robert Dyer, Sambhav Srirama
- *                 and Iowa State University of Science and Technology
+ * Copyright 2014, Hridesh Rajan, Robert Dyer, Sambhav Srirama,
+ *                 Iowa State University of Science and Technology
+ *                 and Bowling Green State University
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,13 +17,6 @@
  */
 package edu.iastate.cs.boa.ui.handlers;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
@@ -30,10 +24,6 @@ import java.util.List;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.equinox.security.storage.ISecurePreferences;
 import org.eclipse.equinox.security.storage.SecurePreferencesFactory;
 import org.eclipse.equinox.security.storage.StorageException;
@@ -143,155 +133,36 @@ public class SubmitToBoaHandler extends AbstractHandler {
 	 * @param client
 	 * @param cache
 	 */
-	@SuppressWarnings("unchecked")
 	public static InputHandle promptDatasetSelection(final ExecutionEvent event, final BoaClient client, final ISecurePreferences cache) {
-		if (startDatasetRefreshThread(cache, client)) {
-			String[] items = new String[0];
-			List<InputHandle> list;
-
- 			try {
- 				final byte[] bytes = cache.getByteArray("list", null);
- 				if (bytes == null) {
- 					showError(event,
- 							"Job submission failed: Unable to obtain list of input datasets\n\n"
- 							+ "Verify your Boa username/password are correct and your internet connection is stable.");
- 					return null;
- 				}
-				list = (List<InputHandle>)bytesToObj(bytes);
- 				if (list == null) {
- 					showError(event,
- 							"Job submission failed: Unable to obtain list of input datasets\n\n"
- 							+ "Verify your Boa username/password are correct and your internet connection is stable.");
- 					return null;
- 				}
-
- 				items = new String[list.size()];
- 				for (int i = 0; i < list.size(); i++)
- 					items[i] = list.get(i).getName();
-			} catch (final StorageException e) {
-				e.printStackTrace();
-				showError(event,
-						"Job submission failed: Unable to obtain list of input datasets: " + e.getLocalizedMessage() + "\n\n"
-						+ "Verify your Boa username/password are correct and your internet connection is stable.");
-				return null;
- 			}
-
-			/*
-			 * Display the dataset choices
-			 */
- 			final InputSelectionDialog dlg = new InputSelectionDialog(HandlerUtil.getActiveWorkbenchWindow(event).getShell(), "Boa", "Select the input dataset to query:", items, items[0], null);
- 			if (dlg.open() == Window.CANCEL)
- 				return null;
-
- 			final String val = dlg.getValue();
- 			for (final InputHandle h : list)
- 				if (h.getName().equals(val))
- 					return h;
-		}
-		return null;
-	}
-
-	private static Object bytesToObj(final byte[] b) {
-		final ByteArrayInputStream bis = new ByteArrayInputStream(b);
-		ObjectInput in = null;
-		try {
-			in = new ObjectInputStream(bis);
-			return in.readObject(); 
-		} catch (final ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (final IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				bis.close();
-			} catch (final IOException ex) {}
-			try {
-				if (in != null) in.close();
-			} catch (final IOException ex) {}
-		}
-		return null;
-	}
-
-	private static byte[] objToBytes(final Object o) {
-		final ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		ObjectOutput out = null;
+		String[] items = new String[0];
+		List<InputHandle> list = null;
 
 		try {
-			out = new ObjectOutputStream(bos);   
-			out.writeObject(o);
-			return bos.toByteArray();
-		} catch (final IOException e) {
+			list = client.getDatasets();
+			items = new String[list.size()];
+			for (int i = 0; i < list.size(); i++)
+				items[i] = list.get(i).getName();
+		} catch (BoaException e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				if (out != null) out.close();
-			} catch (final IOException ex) {}
-			try {
-				bos.close();
-			} catch (final IOException ex) {}
+			showError(event,
+					"Job submission failed: Unable to obtain list of input datasets: " + e.getLocalizedMessage() + "\n\n"
+					+ "Verify your Boa username/password are correct and your internet connection is stable.");
+			return null;
 		}
-		return null;
-	}
-
-	/**
-	 * Starts the Job thread that refreshes the Boa datasets cache. This occurs
-	 * every 2 days after being activated. When refreshing, a progress bar (with
-	 * a percentage) will render in the bottom right of the Eclipse window.
-	 *
-	 * @param cache
-	 * @param client
-	 * @return true if the last refresh cycle was successful, false otherwise
-	 */
-	public static boolean startDatasetRefreshThread(final ISecurePreferences cache, final BoaClient client) {
-		final Job job = new Job("Refreshing Boa Cache") {
-			protected IStatus run(final IProgressMonitor monitor) {
-				try {
-					monitor.beginTask("Refreshing Boa Cache", 3);
-
-					final List<InputHandle> datasets = client.getDatasets();
-					monitor.worked(1);
-
-					try {
-						cache.clear();
-						monitor.worked(1);
-
-						cache.putByteArray("list", objToBytes(datasets), false);
-						monitor.worked(1);
-
-						cache.flush();
-
-						if (monitor.isCanceled())
-							return Status.CANCEL_STATUS;
-					} catch (final StorageException e) {
-						e.printStackTrace();
-						return Status.CANCEL_STATUS;
-					} catch (final IOException e) {
-						e.printStackTrace();
-						return Status.CANCEL_STATUS;
-					} finally {
-						schedule(172800000); // start again in 48 hours
-					}
-					monitor.done();
-					return Status.OK_STATUS;
-				} catch (final BoaException e) {
-					e.printStackTrace();
-					return Status.CANCEL_STATUS;
-				}
- 			}
-		};
 
 		/*
-		 * The following block of code should be used if a process needs to
-		 * happen at the very end of the job.
-		 *
-		 * job.addJobChangeListener(new JobChangeAdapter() { public void
-		 * done(IJobChangeEvent event) { if (!event.getResult().isOK())
-		 * JOptionPane.showMessageDialog(null,
-		 * "Job did not complete successfully"); } });
+		 * Display the dataset choices
 		 */
+		final InputSelectionDialog dlg = new InputSelectionDialog(HandlerUtil.getActiveWorkbenchWindow(event).getShell(), "Boa", "Select the input dataset to query:", items, items[0], null);
+		if (dlg.open() == Window.CANCEL)
+			return null;
 
-		job.schedule(); // start as soon as possible
-		return true;
+		final String val = dlg.getValue();
+		for (final InputHandle h : list)
+			if (h.getName().equals(val))
+				return h;
+
+		return null;
 	}
 
 	private static void showError(final ExecutionEvent event, final String msg) {
